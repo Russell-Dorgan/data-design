b446bc67d64fa4e739b01687187610c247a51e67
Russell Dorgan <clambakedbenz@gmail.com>
2020-03-10 14:33:33 -0600

    Created: Directory: Epic    persona.php    use-case-interaction-flow.php    user-story.php index.html

--- epic/user-story.php
+++ epic/user-story.php
@@ -1,6 +1,6 @@
 <h1> User Story </h1>
 
 <div>
-	<p>Bob, as an enthusiast, is looking to accomplish gaining sufficient knowledge to pursue part time phone repairs</p>
+	<p>Bob, as an enthusiast, is looking to accomplish gaining sufficient knowledge to pursue part time phone repairs to further generate capitol</p>
 
 </div>
