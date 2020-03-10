6914a181e445052b2c99a7a3c35bf71d6bdfb3df
Russell Dorgan <clambakedbenz@gmail.com>
2020-03-10 14:23:50 -0600

    Created: Directory: Epic    persona.php    use-case-interaction-flow.php    user-story.php index.html

--- epic/persona.php
+++ epic/persona.php
@@ -8,6 +8,7 @@
 			<li>Lives alone</li>
 			<li> 2 Dogs</li>
 			<li>Thrives on being in the know regarding new phones and phone updates</li>
+		<img src="index.jpg" alt="Phone">;
 		</ul>
 	</p>
 </div>
