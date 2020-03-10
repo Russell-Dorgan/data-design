d183297a8160d01a51df156b6ea648fbb186190a
Russell Dorgan <clambakedbenz@gmail.com>
2020-03-10 14:32:28 -0600

    Created: Directory: Epic    persona.php    use-case-interaction-flow.php    user-story.php index.html

--- epic/use-case-interaction-flow.php
+++ epic/use-case-interaction-flow.php
@@ -3,4 +3,9 @@
 
 <div>
 	<p>Bob wakes up in the morning at 8 AM and browses the various subreddits to further keep up to date on all the latest changes in trends with all doing in the mobile world.</p>
+	<p>Specific steps Bob would have to take to arrive at his goal would be:</p>
+	<ul>
+		<li>Continuing his routine without falter(Doing proper research at a certain time every day)</li>
+		<li>Messaging some more tenured individuals on the forum to pick their brain about certain subjects</li>
+	</ul>
 </div>
